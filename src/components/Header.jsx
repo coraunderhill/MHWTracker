085124fa753e66e12017e2786e9058461d146743
Cora Underhill <coraunderhill@gmail.com>
2024-02-26 22:26:16 -0800

    Added Header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+/**
+ * Application header
+ * @returns {React.JSX.Element}
+ */
+const Header = () => {
+
+  return (
+    <h1>Monster Hunter World Armor Tracker</h1>
+  );
+
+}
+
+export default Header;
